e67a5c4b5b3e71e7c0d93dca139a38b6d5f4eed5
Ollie Terrance <ollie.terrance@live.co.uk>
2015-08-03 22:27:14 +0100

    Title and paragraph insertion, backspace

--- BootPolish/res/js/bp.js
+++ BootPolish/res/js/bp.js
@@ -1,13 +1,42 @@
 $(document).ready(function(e) {
     var $lenses;
-    function addLens(node) {
-        $(node).append($("<div>").addClass("lens"));
+    function addLens(node, pos) {
+        var lens = $("<div>").addClass("lens");
+        switch (pos) {
+            case -1:
+                $(node).before(lens);
+                break;
+            case 0:
+                $(node).append(lens);
+                break;
+            case 1:
+                $(node).after(lens);
+                break;
+        }
         $lenses = $(".lens");
     }
-    addLens("#bp-root");
+    addLens("#bp-root", 0);
     $lenses.first().addClass("focus");
     Mousetrap.bind(["left", "right"], function(e, key) {
         var pos = $lenses.index($(".lens.focus").removeClass("focus"));
         $($lenses.get((pos + (key === "left" ? -1 : 1)) % $lenses.length)).addClass("focus");
+    }).bind("backspace", function(e, key) {
+        var pos = $lenses.index($(".lens.focus"));
+        if (pos === 0) return;
+        var $all = $("#bp-root *");
+        var start = $all.index($lenses.get(pos - 1));
+        var end = $all.index($lenses.get(pos));
+        $all.slice(start, end).remove();
+        $lenses = $(".lens");
+    }).bind(["1", "2", "3", "4", "5", "6"], function(e, key) {
+        var title = prompt("Title:", "");
+        if (title === null) return;
+        addLens(".lens.focus", -1);
+        $("<h" + key + ">").html(title).insertBefore(".lens.focus");
+    }).bind("p", function(e, key) {
+        var para = prompt("Paragraph:", "");
+        if (para === null) return;
+        addLens(".lens.focus", -1);
+        $("<p>").html(para).insertBefore(".lens.focus");
     });
 });
