ce8227babe58f16ef57e427c22af94a6763008b3
Ollie Terrance <ollie.terrance@live.co.uk>
2015-08-04 23:35:43 +0100

    Wells and panels

--- BootPolish/res/js/bp.js
+++ BootPolish/res/js/bp.js
@@ -25,6 +25,7 @@ $(document).ready(function(e) {
     }).bind("\\", function(e, key) {
         $lenses.show();
     }, "keyup").bind("backspace", function(e, key) {
+        e.preventDefault();
         $(".lens.focus").prevUntil(".lens").prev().andSelf().remove();
         $lenses = $(".lens");
     }).bind("shift+backspace", function(e, key) {
@@ -35,6 +36,11 @@ $(document).ready(function(e) {
         var text = prompt("Raw text:", "");
         if (!text) return;
         $(".lens.focus").before($("<div>").addClass("lens")).before(text);
+        $lenses = $(".lens");
+    }).bind("h", function(e, key) {
+        var header = prompt("Page header:", "");
+        if (!header) return;
+        $(".lens.focus").before($("<div>").addClass("lens")).before($("<div>").addClass("page-header").append($("<h1>").html(header)));
         $lenses = $(".lens");
     }).bind(["1", "2", "3", "4", "5", "6"], function(e, key) {
         var title = prompt("Title (level " + key + "):", "");
@@ -47,29 +53,52 @@ $(document).ready(function(e) {
         $(".lens.focus").before($("<div>").addClass("lens")).before($("<p>").toggleClass("lead", key === "shift+p").html(para));
         $lenses = $(".lens");
     }).bind("a", function(e, key) {
-        var level = prompt("Alert level (success/info/warning/danger):", "");
-        if (level === null || ["success", "info", "warning", "danger"].indexOf(level) === -1) return;
-        $(".lens.focus").before($("<div>").addClass("lens")).before($("<div>").addClass("alert alert-" + level).append($("<div>").addClass("lens focus"))).removeClass("focus");
+        var colour = prompt("Alert colour (success/info/warning/danger):", "info");
+        if (["success", "info", "warning", "danger"].indexOf(colour) === -1) return;
+        $(".lens.focus").before($("<div>").addClass("lens")).before($("<div>").addClass("alert alert-" + colour).append($("<div>").addClass("lens focus"))).removeClass("focus");
         $lenses = $(".lens");
-    }).bind("d", function(e, key) {
+    }).bind(["w", "shift+w", "shift+alt+w"], function(e, key) {
+        var size = (key === "w" ? "" : (key === "shift+w" ? " well-lg" : " well-sm"));
+        $(".lens.focus").before($("<div>").addClass("lens")).before($("<div>").addClass("well" + size).append($("<div>").addClass("lens focus"))).removeClass("focus");
+        $lenses = $(".lens");
+    }).bind(["l"], function(e, key) {
+        var title = prompt("Panel heading (prefix '!' for title, blank for none):", "");
+        if (title === null) return;
+        var footer = confirm("Add footer?");
+        var colour = prompt("Panel colour (default/primary/success/info/warning/danger):", "default");
+        if (["default", "primary", "success", "info", "warning", "danger"].indexOf(colour) === -1) return;
+        var $panel = $("<div>").addClass("panel panel-" + colour);
+        if (title) {
+            var $heading = $("<div>").addClass("panel-heading").text(title);
+            if (title[0] === "!") $heading.empty().append($("<h3>").addClass("panel-title").text(title.substr(1)));
+            $panel.append($heading);
+        }
+        $panel.append($("<div>").addClass("panel-body").append($("<div>").addClass("lens focus")));
+        if (footer) $panel.append($("<div>").addClass("panel-footer").append($("<div>").addClass("lens")));
+        $(".lens.focus").before($("<div>").addClass("lens")).before($panel).removeClass("focus");
+        $lenses = $(".lens");
+    }).bind(["d", "shift+d"], function(e, key) {
         var label = prompt("Button label:", "");
         if (label === null) return;
+        var colour = prompt("Button colour (default/primary/success/info/warning/danger):", "default");
+        if (["default", "primary", "success", "info", "warning", "danger"].indexOf(colour) === -1) return;
+        var up = (key === "shift+d");
         var opts = [];
         while (true) {
-            var opt = prompt("Add option ('--' for separator, prefix '@' for heading, blank to end):", "");
+            var opt = prompt("Add option ('--' for separator, prefix '!' for heading, blank to end):", "");
             if (opt === null) return;
             if (opt === "") break;
             if (opt === "--") {
                 opts.push($("<li>").addClass("divider").attr("role", "separator"));
-            } else if (opt[0] === "@") {
+            } else if (opt[0] === "!") {
                 opts.push($("<li>").addClass("dropdown-header").text(opt.substr(1)));
             } else {
                 opts.push($("<li>").append($("<a>").text(opt)));
             }
         }
         var $dropdown = $("<ul>").addClass("dropdown-menu").append(opts);
-        var $button = $("<button>").addClass("btn btn-default dropdown-toggle").attr("data-toggle", "dropdown").text(label + " ").append($("<span>").addClass("caret"));
-        var $root = $("<div>").addClass("dropdown").append($button).append($dropdown);
+        var $button = $("<button>").addClass("btn btn-" + colour + " dropdown-toggle").attr("data-toggle", "dropdown").text(label + " ").append($("<span>").addClass("caret"));
+        var $root = $("<div>").addClass("drop" + (up ? "up" : "down")).append($button).append($dropdown);
         $(".lens.focus").before($("<div>").addClass("lens")).before($root);
         $lenses = $(".lens");
     }).bind("t", function(e, key) {
