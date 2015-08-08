d056c7b7a39eeb7a282d9397cbfdc3fbd9b05caa
Ollie Terrance <ollie.terrance@live.co.uk>
2015-08-08 11:09:21 +0100

    Complete API class conversion

--- BootPolish/res/js/bp.js
+++ BootPolish/res/js/bp.js
@@ -1,5 +1,6 @@
+"use strict";
 var BootPolish = function(root) {
-    this.$root = $(root);
+    this.$root = $(root).addClass("bp-edit");
     this.$root.append(this.newLens(true));
     this.syncLenses();
 };
@@ -19,7 +20,7 @@ $.extend(BootPolish.prototype, {
         return $(".lens.focus", this.$root);
     },
     showEdit: function(show) {
-        this.$root.toggleClass("edit", show);
+        this.$root.toggleClass("bp-edit", show);
     },
     nav: function(fwd) {
         var pos = this.$lenses.index(this.focus().removeClass("focus"));
@@ -47,11 +48,11 @@ $.extend(BootPolish.prototype, {
         }
         this.syncLenses();
     },
-    createHeading: function(block, size, text) {
+    createHeading: function(text, block, size) {
         var $title = $("<h" + size + ">").html(text);
         this.insert(block ? $("<div>").addClass("page-header").append($title) : $title);
     },
-    createParagraph: function(lead, text) {
+    createParagraph: function(text, lead) {
         this.insert($("<p>").toggleClass("lead", lead).html(text));
     },
     createJumbotron: function() {
@@ -66,109 +67,33 @@ $.extend(BootPolish.prototype, {
         }
         this.insert($alert.append(this.newLens()));
     },
-    createWell: function(size) {
-        this.insert($("<div>").addClass("well" + (size ? " well-" + size : "")).append(this.newLens()));
-    }
-});
-$(document).ready(function(e) {
-    window.bp = new BootPolish("#bp-root");
-    Mousetrap.bind(["left", "right"], function(e, key) {
-        bp.nav(key === "left" ? -1 : 1);
-    }).bind("\\", function(e, key) {
-        bp.showEdit(false);
-    }).bind("\\", function(e, key) {
-        bp.showEdit(true);
-    }, "keyup").bind("backspace", function(e, key) {
-        e.preventDefault();
-        bp.del();
-    }).bind("shift+backspace", function(e, key) {
-        if (confirm("Clear all sibling nodes?")) bp.delSiblings();
-    }).bind("enter", function(e, key) {
-        var text = prompt("Raw text:", "");
-        if (!text) return;
-        bp.insert(text);
-    }).bind("j", function(e, key) {
-        bp.createJumbotron();
-    }).bind(["1", "2", "3", "4", "5", "6", "shift+1", "shift+2", "shift+3", "shift+4", "shift+5", "shift+6"], function(e, key) {
-        var parts = key.split("+");
-        var block = (parts.length === 2);
-        if (block) parts.shift();
-        var size = parts[0];
-        var text = prompt("Title (level " + size + "):", "");
-        if (!text) return;
-        bp.createHeading(block, size, text);
-    }).bind(["p", "shift+p"], function(e, key) {
-        var text = prompt((key === "p" ? "P" : "Lead p") + "aragraph:", "");
-        if (!text) return;
-        bp.createParagraph(key === "shift+p", text);
-    }).bind("a", function(e, key) {
-        var colour = prompt("Alert colour (success/info/warning/danger):", "info");
-        if (["success", "info", "warning", "danger"].indexOf(colour) === -1) return;
-        var dismissable = confirm("Dismissable?");
-        bp.createAlert(dismissable, colour);
-    }).bind(["w", "shift+w", "alt+w"], function(e, key) {
-        var size = prompt("Well size (lg/sm, blank for normal):", "");
-        if (["lg", "sm", ""].indexOf(size) === -1) return;
-        bp.createWell(size);
-    }).bind("l", function(e, key) {
-        var title = prompt("Panel heading (prefix '!' for title, blank for none):", "");
-        if (title === null) return;
-        var footer = confirm("Add footer?");
-        var colour = prompt("Panel colour (default/primary/success/info/warning/danger):", "default");
-        if (["default", "primary", "success", "info", "warning", "danger"].indexOf(colour) === -1) return;
+    createPanel: function(title, asHeading, footer, colour) {
         var $panel = $("<div>").addClass("panel panel-" + colour);
         if (title) {
-            var $heading = $("<div>").addClass("panel-heading").text(title);
-            if (title[0] === "!") $heading.empty().append($("<h3>").addClass("panel-title").text(title.substr(1)));
+            var $heading = $("<div>").addClass("panel-heading");
+            $heading.append(asHeading ? $("<h3>").addClass("panel-title").text(title) : title);
             $panel.append($heading);
         }
-        $panel.append($("<div>").addClass("panel-body").append($("<div>").addClass("lens focus")));
-        if (footer) $panel.append($("<div>").addClass("panel-footer").append($("<div>").addClass("lens")));
-        $(".lens.focus").before($("<div>").addClass("lens")).before($panel).removeClass("focus");
-        bp.syncLenses();
-    }).bind("b b", function(e, key) {
-        var label = prompt("Button label:", "");
-        if (!label) return;
-        var size = prompt("Button size (lg/sm/xs, blank for normal):", "");
-        if (["lg", "sm", "xs", ""].indexOf(size) === -1) return;
-        var colour = prompt("Button colour (default/primary/success/info/warning/danger):", "default");
-        if (["default", "primary", "success", "info", "warning", "danger"].indexOf(colour) === -1) return;
-        $(".lens.focus").before($("<div>").addClass("lens")).before($("<button>").addClass("btn" + (size ? " btn-" + size : "") + " btn-" + colour).text(label));
-        bp.syncLenses();
-    }).bind(["b d", "b shift+d"], function(e, key) {
-        var label = prompt("Button label:", "");
-        if (label === null) return;
-        var size = prompt("Button size (lg/sm/xs, blank for normal):", "");
-        if (["lg", "sm", "xs", ""].indexOf(size) === -1) return;
-        var colour = prompt("Button colour (default/primary/success/info/warning/danger):", "default");
-        if (["default", "primary", "success", "info", "warning", "danger"].indexOf(colour) === -1) return;
-        var up = (key === "b shift+d");
-        var opts = [];
-        while (true) {
-            var opt = prompt("Add option ('--' for separator, prefix '!' for heading, blank to end):", "");
-            if (opt === null) return;
-            if (opt === "") break;
-            if (opt === "--") {
-                opts.push($("<li>").addClass("divider").attr("role", "separator"));
-            } else if (opt[0] === "!") {
-                opts.push($("<li>").addClass("dropdown-header").text(opt.substr(1)));
-            } else {
-                opts.push($("<li>").append($("<a>").text(opt)));
-            }
+        $panel.append($("<div>").addClass("panel-body").append(this.newLens()));
+        if (footer) $panel.append($("<div>").addClass("panel-footer").append(this.newLens()));
+        this.insert($panel);
+    },
+    createWell: function(size) {
+        this.insert($("<div>").addClass("well" + (size ? " well-" + size : "")).append(this.newLens()));
+    },
+    createButton: function(label, colour, size, dropdown) {
+        if (dropdown) {
+            var $menu = $("<ul>").addClass("dropdown-menu").append(dropdown.elements);
+            var $button = $("<button>").addClass("btn" + (size ? " btn-" + size : "") + " btn-" + colour + " dropdown-toggle").attr("data-toggle", "dropdown")
+                    .text(label + " ").append($("<span>").addClass("caret"));
+            this.insert($("<div>").addClass("drop" + dropdown.direction).append($button).append($menu));
+        } else {
+            this.insert($("<button>").addClass("btn" + (size ? " btn-" + size : "") + " btn-" + colour).text(label));
         }
-        var $dropdown = $("<ul>").addClass("dropdown-menu").append(opts);
-        var $button = $("<button>").addClass("btn" + (size ? " btn-" + size : "") + " btn-" + colour + " dropdown-toggle").attr("data-toggle", "dropdown").text(label + " ").append($("<span>").addClass("caret"));
-        var $root = $("<div>").addClass("drop" + (up ? "up" : "down")).append($button).append($dropdown);
-        $(".lens.focus").before($("<div>").addClass("lens")).before($root);
-        bp.syncLenses();
-    }).bind("t", function(e, key) {
-        var rows = parseInt(prompt("Rows:", ""));
-        if (!rows || rows < 0) return;
-        var cols = parseInt(prompt("Columns:", ""));
-        if (!cols || cols < 0) return;
-        var header = rows > 1 && confirm("First row as header?");
+    },
+    createTable: function(rows, cols, asHeader) {
         var $table = $("<table>").addClass("table table-bordered");
-        if (header) {
+        if (asHeader) {
             $row = $("<tr>");
             for (var j = 0; j < cols; j++) {
                 var $cell = $("<th>");
@@ -188,41 +113,47 @@ $(document).ready(function(e) {
             }
             $tbody.append($row);
         }
-        $table.append($tbody);
-        $(".lens.focus").before($("<div>").addClass("lens")).before($table);
-        bp.syncLenses();
-    }).bind("f f", function(e, key) {
-        $(".lens.focus").before($("<div>").addClass("lens")).before($("<form>").append($("<div>").addClass("lens focus"))).removeClass("focus");
-        bp.syncLenses();
-    }).bind(["f i", "f e", "f n", "f p"], function(e, key) {
-        var placeholder = prompt("Placeholder:", "");
-        if (placeholder === null) return;
-        var type = (key === "f i" ? "text" : (key === "f e" ? "email" : (key === "f n" ? "number" : "password")));
-        $(".lens.focus").before($("<div>").addClass("lens")).before($("<input>").attr("type", type).attr("placeholder", placeholder || undefined).addClass("form-control"));
-        bp.syncLenses();
-    }).bind("f d", function(e, key) {
-        var opts = [];
-        while (true) {
-            var opt = prompt("Add option (blank to end):", "");
-            if (opt === null) return;
-            if (opt === "") break;
-            opts.push(opt);
+        this.insert($table.append($tbody));
+    },
+    createForm: function(horiz) {
+        this.insert($("<form>").toggleClass("form-horizontal", horiz).append(this.newLens()));
+    },
+    createFormGroup: function() {
+        this.insert($("<div>").addClass("form-group").append(this.newLens()));
+    },
+    createFormLabel: function(label) {
+        this.insert($("<label>").text(label));
+    },
+    createFormInput: function(type, placeholder) {
+        this.insert($("<input>").attr("type", type).addClass("form-control").attr("placeholder", placeholder));
+    },
+    createFormDropdown: function(dropdown) {
+        this.insert($("<select>").addClass("form-control").append(dropdown.elements));
+    },
+    createFormButton: function(type, label, colour, size) {
+        this.insert($("<input>").attr("type", type).addClass("btn" + (size ? " btn-" + size : "") + " btn-" + colour).val(label));
+    }
+});
+BootPolish.Dropdown = function(native) {
+    this.elements = [];
+    this.direction = "down";
+    this.native = native;
+}
+$.extend(BootPolish.Dropdown.prototype, {
+    setDropup: function(up) {
+        this.direction = (up ? "up" : "down");
+    },
+    addSeparator: function() {
+        if (!this.native) this.elements.push($("<li>").addClass("divider").attr("role", "separator"));
+    },
+    addHeading: function(text) {
+        if (!this.native) this.elements.push($("<li>").addClass("dropdown-header").text(text));
+    },
+    addOption: function(text, url) {
+        if (this.native) {
+            this.elements.push($("<option>").text(text));
+        } else {
+            this.elements.push($("<li>").append($("<a>").attr("href", url).text(text)));
         }
-        var $select = $("<select>").addClass("form-control");
-        for (var i in opts) {
-            $select.append($("<option>").text(opts[i]));
-        }
-        $(".lens.focus").before($("<div>").addClass("lens")).before($select);
-        bp.syncLenses();
-    }).bind("f l", function(e, key) {
-        var label = prompt("Label:", "");
-        if (!label) return;
-        $(".lens.focus").before($("<div>").addClass("lens")).before($("<label>").text(label));
-        bp.syncLenses();
-    }).bind("f s", function(e, key) {
-        var label = prompt("Submit label:", "Submit");
-        if (!label) return;
-        $(".lens.focus").before($("<div>").addClass("lens")).before($("<input>").attr("type", "submit").addClass("btn btn-primary").val(label));
-        bp.syncLenses();
-    });
+    }
 });
---
+++ BootPolish/res/js/index.js
@@ -0,0 +1,118 @@
+"use strict";
+$(document).ready(function(e) {
+    window.bp = new BootPolish("#bp-root");
+    Mousetrap.bind(["left", "right"], function(e, key) {
+        bp.nav(key === "left" ? -1 : 1);
+    }).bind("\\", function(e, key) {
+        bp.showEdit(false);
+    }).bind("\\", function(e, key) {
+        bp.showEdit(true);
+    }, "keyup").bind("backspace", function(e, key) {
+        e.preventDefault();
+        bp.del();
+    }).bind("shift+backspace", function(e, key) {
+        if (confirm("Clear all sibling nodes?")) bp.delSiblings();
+    }).bind("enter", function(e, key) {
+        var text = prompt("Raw text:", "");
+        if (!text) return;
+        bp.insert(text);
+    }).bind("j", function(e, key) {
+        bp.createJumbotron();
+    }).bind(["1", "2", "3", "4", "5", "6", "shift+1", "shift+2", "shift+3", "shift+4", "shift+5", "shift+6"], function(e, key) {
+        var parts = key.split("+");
+        var block = (parts.length === 2);
+        if (block) parts.shift();
+        var size = parts[0];
+        var text = prompt("Title (level " + size + "):", "");
+        if (!text) return;
+        bp.createHeading(text, block, size);
+    }).bind(["p", "shift+p"], function(e, key) {
+        var text = prompt((key === "p" ? "P" : "Lead p") + "aragraph:", "");
+        if (!text) return;
+        bp.createParagraph(text, key === "shift+p");
+    }).bind("a", function(e, key) {
+        var colour = prompt("Alert colour (success/info/warning/danger):", "info");
+        if (["success", "info", "warning", "danger"].indexOf(colour) === -1) return;
+        var dismissable = confirm("Dismissable?");
+        bp.createAlert(dismissable, colour);
+    }).bind(["w", "shift+w", "alt+w"], function(e, key) {
+        var size = prompt("Well size (lg/sm, blank for normal):", "");
+        if (["lg", "sm", ""].indexOf(size) === -1) return;
+        bp.createWell(size);
+    }).bind("l", function(e, key) {
+        var title = prompt("Panel heading (prefix '!' for title, blank for none):", "");
+        if (title === null) return;
+        var footer = confirm("Add footer?");
+        var colour = prompt("Panel colour (default/primary/success/info/warning/danger):", "default");
+        if (["default", "primary", "success", "info", "warning", "danger"].indexOf(colour) === -1) return;
+        bp.createPanel(title.substr(title[0] === "!" ? 1 : 0), title[0] === "!", footer, colour);
+    }).bind(["b b", "b d", "b shift+d"], function(e, key) {
+        var label = prompt("Button label:", "");
+        if (label === null) return;
+        var size = prompt("Button size (lg/sm/xs, blank for normal):", "");
+        if (["lg", "sm", "xs", ""].indexOf(size) === -1) return;
+        var colour = prompt("Button colour (default/primary/success/info/warning/danger):", "default");
+        if (["default", "primary", "success", "info", "warning", "danger"].indexOf(colour) === -1) return;
+        var dropdown;
+        if (key !== "b b") {
+            dropdown = new BootPolish.Dropdown(false);
+            dropdown.setDropup(key === "b shift+d");
+            while (true) {
+                var opt = prompt("Add option ('--' for separator, prefix '!' for heading, blank to end):", "");
+                if (opt === null) return;
+                if (opt === "") break;
+                if (opt === "--") {
+                    dropdown.addSeparator();
+                } else if (opt[0] === "!") {
+                    dropdown.addHeading(opt.substr(1));
+                } else {
+                    dropdown.addOption(opt);
+                }
+            }
+        }
+        bp.createButton(label, colour, size, dropdown);
+    }).bind("t", function(e, key) {
+        var rows = parseInt(prompt("Rows:", ""));
+        if (!rows || rows < 0) return;
+        var cols = parseInt(prompt("Columns:", ""));
+        if (!cols || cols < 0) return;
+        var header = rows > 1 && confirm("First row as header?");
+        bp.createTable(rows, cols, header);
+    }).bind("f f", function(e, key) {
+        bp.createForm(confirm("Horizontal?"));
+    }).bind("f g", function(e, key) {
+        bp.createFormGroup();
+    }).bind("f l", function(e, key) {
+        var label = prompt("Label:", "");
+        if (!label) return;
+        bp.createFormLabel(label);
+    }).bind("f i", function(e, key) {
+        var type = prompt("Input type (text/password/email/number/date/time):", "text");
+        if (["text", "password", "email", "number", "date", "time"].indexOf(type) === -1) return;
+        var placeholder;
+        if (["text", "password", "email", "number"].indexOf(type) >= 0) {
+            placeholder = prompt("Placeholder:", "");
+            if (placeholder === null) return;
+        }
+        bp.createFormInput(type, placeholder);
+    }).bind("f d", function(e, key) {
+        var dropdown = new BootPolish.Dropdown(true);
+        while (true) {
+            var opt = prompt("Add option (blank to end):", "");
+            if (opt === null) return;
+            if (opt === "") break;
+            dropdown.addOption(opt);
+        }
+        bp.createFormDropdown(dropdown);
+    }).bind("f b", function(e, key) {
+        var type = prompt("Input type (submit/reset/button):", "submit");
+        if (["submit", "reset", "button"].indexOf(type) === -1) return;
+        var label = prompt("Button label:", "");
+        if (label === null) return;
+        var size = prompt("Button size (lg/sm/xs, blank for normal):", "");
+        if (["lg", "sm", "xs", ""].indexOf(size) === -1) return;
+        var colour = prompt("Button colour (default/primary/success/info/warning/danger):", "default");
+        if (["default", "primary", "success", "info", "warning", "danger"].indexOf(colour) === -1) return;
+        bp.createFormButton(type, label, colour, size);
+    });
+});
